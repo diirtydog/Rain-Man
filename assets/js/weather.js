68e08b77a4cee804e2f59c47ec6b2aaa65dc2d64
andrew wales <andrew.derek.wales@gmail.com>
2021-12-15 10:41:14 -0600

    added the form to search for city

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -5,7 +5,7 @@
 // api key is b3e2bf242500fea051a4efa17da1cf49
 // api structure is api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // create the function that fetches the data from the third party api
-var getWeatherRepos = function () {
+var getWeatherRepos = function (city) {
     // format the open weather api url
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b3e2bf242500fea051a4efa17da1cf49";
 
@@ -24,5 +24,22 @@ var getWeatherRepos = function () {
     })
     console.log('function was called ho')
 };
+var cityFormEl = document.querySelector('#city-form');
+var cityInputEl = document.querySelector('#city-name');
 
-getWeatherRepos();
+var formSubmitHandler = function (event) {
+    event.preventDefault();
+    // get value from input element
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+        getWeatherRepos(city);
+        cityInputEl.value = '';
+    } else {
+        alert('Please enter a city Name');
+    }
+    console.log(event);
+};
+
+// add event listener for the submit button 
+cityFormEl = addEventListener('submit', formSubmitHandler);
