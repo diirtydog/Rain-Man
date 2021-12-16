5e52dc7fa6815bc224fad0728e7d1b694c5a19a8
diirtydog <andrew.derek.wales@gmail.com>
2021-12-15 21:56:27 -0600

    added second api to get longtitude and latitude

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,19 +2,35 @@
 var weatherContainerEl = document.querySelector('#weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
 
-
+// geo api  http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 // api key is b3e2bf242500fea051a4efa17da1cf49
 // api structure is api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 // create the function that fetches the data from the third party api
-var getWeatherRepos = function (city) {
-    // format the open weather api url
+var getLongLat = function (city) {
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=b3e2bf242500fea051a4efa17da1cf49";
+
+    // make fetch
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                getWeatherRepos(data, city)
+            })
+        }
+    })
+    console.log(city.coord.lon);
+   
+};
+
+var getWeatherRepos = function (lat, lon) {
+    //console.log
+    // format the open weather api url
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=b3e2bf242500fea051a4efa17da1cf49";
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                displayWeather(data, city);
+                displayWeather(data, lat, lon);
             });
         } else {
             alert("Error: City Not Found Check Spelling!");
@@ -34,7 +50,7 @@ var formSubmitHandler = function (event) {
     var city = cityInputEl.value.trim();
 
     if (city) {
-        getWeatherRepos(city);
+        getLongLat(city);
         cityInputEl.value = '';
     } else {
         alert('Please enter a city Name');
@@ -57,6 +73,8 @@ var displayWeather = function(city, searchTerm) {
     console.log(city);
     console.log(searchTerm);
 }
-
+//console.log(latitude, longtitude)
 // add event listener for the submit button 
 cityFormEl = addEventListener('submit', formSubmitHandler);
+
+
