20ebaa316bf5b30e43db8abc9478b72c1e2d177a
diirtydog <andrew.derek.wales@gmail.com>
2021-12-19 10:32:25 -0600

    Made display weather function and started future

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -2,6 +2,7 @@
 var weatherContainerEl = document.querySelector('#weather-container');
 var weatherSearchTerm = document.querySelector('#weather-search-term');
 
+
 // geo api  http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 // api key is b3e2bf242500fea051a4efa17da1cf49
 // api structure is api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
@@ -13,24 +14,31 @@ var getLongLat = function (city) {
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                getWeatherRepos(data, city)
+                getLatLon(data, city)
+                //console.log(data.coord.lon)
             })
         }
     })
-    console.log(city.coord.lon);
+   
    
 };
-
+function getLatLon(data) {
+    lat = data.coord.lat
+    lon = data.coord.lon
+    getWeatherRepos(lat, lon)
+}
 var getWeatherRepos = function (lat, lon) {
-    //console.log
+    //console.log(lat, lon)
     // format the open weather api url
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=b3e2bf242500fea051a4efa17da1cf49";
+        var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=b3e2bf242500fea051a4efa17da1cf49";
 
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
-                displayWeather(data, lat, lon);
+                displayWeather(data);
+                futureWeather(data);
+                //console.log(data)
             });
         } else {
             alert("Error: City Not Found Check Spelling!");
@@ -51,28 +59,74 @@ var formSubmitHandler = function (event) {
 
     if (city) {
         getLongLat(city);
+        //cityName(cityInputEl.value);
         cityInputEl.value = '';
     } else {
         alert('Please enter a city Name');
     }
     console.log(event);
 };
+
+
 // display weather cards
-var displayWeather = function(city, searchTerm) {
+var displayWeather = function(city, cityInputEl) {
+    // console.log(cityInputEl)
     // check if we pulled weather data
     if (city.length === 0) {
         weatherContainerEl.textContent = "City not found.";
         return;
     }
-    weatherContainerEl.textContent = "";
-    weatherSearchTerm.textContent = searchTerm;
-    // loop over weather data
-    for (var i = 0; i < city.length; i++) {
-        
-    }
-    console.log(city);
-    console.log(searchTerm);
+    //var myDate = new Date( your epoch date *1000);
+    //document.write(myDate.toGMTString()+"<br>"+myDate.toLocaleString());
+    var weatherCard = document.getElementById("weather-container");
+    var {dt, temp, humidity, uvi, wind_speed}= city.current;
+
+    // var cityName = document.createElement('h2')
+    // cityName.textContent = cityInputEl.value;
+
+    var currentDate = document.createElement("li");
+    currentDate.textContent = "Date: " + JSON.stringify(dt);
+
+    var tempCurrent = document.createElement("li");
+    tempCurrent.textContent = "Current Temp: " + JSON.stringify(temp);
+
+    var humidityCurrent = document.createElement("li");
+    humidityCurrent.textContent = "Humidity: " + JSON.stringify(humidity);
+
+    var uvCurrent = document.createElement("li");
+    uvCurrent.textContent = "UV Index: " + JSON.stringify(uvi);
+
+    var windCurrent = document.createElement("li");
+    windCurrent.textContent = "Wind Speed: " + JSON.stringify(wind_speed);
+    
+    // console.log('#city-name'.value);
+
+    //weatherCard.appendChild(cityName);
+    weatherCard.appendChild(currentDate);
+    weatherCard.appendChild(tempCurrent);
+    weatherCard.appendChild(humidityCurrent);
+    weatherCard.appendChild(uvCurrent);
+    weatherCard.appendChild(windCurrent);
+
+    
+    //weatherContainerEl.textContent = "";
+    //weatherSearchTerm.textContent = city;
+    //console.log(JSON.stringify(tempCurrent))
 }
+
+var futureWeather = function (data) {
+    console.log(daily[i])
+    var future = document.getElementById('future');
+    var {dt, weather, temp, wind_speed, humidity}= daily[i];
+
+    var futureDate = document.createElement("li");
+    futureDate.textContent = 'Date: ' + JSON.stringify(dt);
+
+    var futureConditions = document.createElement('li');
+    futureConditions.textcontent = weather[i].icon;
+}
+// Celsius = Kelvin - 273.15
+// Fahrenheit = Kelvin x 1.8 - 459.67
 //console.log(latitude, longtitude)
 // add event listener for the submit button 
 cityFormEl = addEventListener('submit', formSubmitHandler);
